5f832a8ffa566b36ea92a28df36cd35e9214c01a
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-10 15:07:56 +0300

    roll dice

---
+++ P-RollDice-RP/src/components/Dice/Dice.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Dice.css";
+
+const Dice = ({ face, rolling }) => {
+  return (
+  <i className={`dice fas fa-dice-${face} ${rolling && "shaking"} `}></i>
+  );
+};
+
+export default Dice;
