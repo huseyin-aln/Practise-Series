dbea50ebccc8392a85fbc54982ad8f435b9b3c63
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-12 12:25:48 +0300

    income tracker v1

---
+++ P-IncomeTracker-RP/src/components/IncomeForm.jsx
@@ -0,0 +1,55 @@
+import { useRef } from "react";
+
+const IncomeForm = ({ income, setIncome }) => {
+  const desc = useRef(null);
+  const date = useRef(null);
+  const price = useRef(null);
+
+  const addIncome = (e) => {
+    e.preventDefault();
+    setIncome([
+      ...income,
+      {
+        id: Math.floor(Math.random() * 1000),
+        desc: desc.current.value,
+        price: price.current.value,
+        date: date.current.value,
+      },
+    ]);
+
+    desc.current.value = "";
+    price.current.value = "";
+    date.current.value = "";
+  };
+
+  return (
+    <form className="income-form" onSubmit={addIncome}>
+      <div className="form-inner">
+        <input
+          type="text"
+          name="desc"
+          id="desc"
+          placeholder="Income Description..."
+          ref={desc}
+        />
+        <input
+          type="number"
+          name="price"
+          id="price"
+          placeholder="Price..."
+          ref={price}
+        />
+        <input
+          type="date"
+          name="date"
+          id="date"
+          placeholder="Income Date..."
+          ref={date}
+        />
+        <input className="btn" type="submit" value="Add Income" />
+      </div>
+    </form>
+  );
+};
+
+export default IncomeForm;
